01adb50fb70a3c645ddd1d6123b4f1276a7fbdcc
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-02-07 02:46:48 +0600

    solved task 4

--- problems.js
+++ problems.js
@@ -49,3 +49,25 @@ function countVowels(statement) {
 const statement = "Hello programmers.";
 const vowels = countVowels(statement);
 console.log("Total Vowels: " + vowels);
+
+console.log(
+    "=================================================================="
+  );
+  
+  //Task-4:
+  function findLongestWord(statement) {
+    let words = statement.split(' ');
+    let wordSize = words[0].length;
+    let longestWord;
+    for (let i=0; i<words.length; i++) {
+        let long = words[i].length;
+        if(long > wordSize){
+            wordSize = words[i].length;
+            longestWord = words[i];
+        }
+    }
+    return longestWord;
+  }
+  const sentence = "I am learning Programming to become a programmer";
+  const longestWord = findLongestWord(sentence);
+  console.log(longestWord);
