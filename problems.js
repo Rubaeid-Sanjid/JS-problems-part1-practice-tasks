12fbdfe47bf0a57aca48d0f94cb0a7be305ac580
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-02-07 19:30:58 +0600

    solved task 5

--- problems.js
+++ problems.js
@@ -51,23 +51,38 @@ const vowels = countVowels(statement);
 console.log("Total Vowels: " + vowels);
 
 console.log(
-    "=================================================================="
-  );
-  
-  //Task-4:
-  function findLongestWord(statement) {
-    let words = statement.split(' ');
-    let wordSize = words[0].length;
-    let longestWord;
-    for (let i=0; i<words.length; i++) {
-        let long = words[i].length;
-        if(long > wordSize){
-            wordSize = words[i].length;
-            longestWord = words[i];
-        }
+  "=================================================================="
+);
+
+//Task-4:
+function findLongestWord(statement) {
+  let words = statement.split(" ");
+  let wordSize = words[0].length;
+  let longestWord;
+  for (let i = 0; i < words.length; i++) {
+    let long = words[i].length;
+    if (long > wordSize) {
+      wordSize = words[i].length;
+      longestWord = words[i];
     }
-    return longestWord;
   }
-  const sentence = "I am learning Programming to become a programmer";
-  const longestWord = findLongestWord(sentence);
-  console.log(longestWord);
+  return longestWord;
+}
+const sentence = "I am learning Programming to become a programmer";
+const longestWord = findLongestWord(sentence);
+console.log(longestWord);
+
+console.log(
+  "=================================================================="
+);
+
+// Task-5:
+function randomNumberGenerator() {
+  const randomNumber = Math.floor(Math.random() * 20) + 10;
+  console.log(randomNumber);
+}
+randomNumberGenerator();
+
+console.log(
+  "=================================================================="
+);
